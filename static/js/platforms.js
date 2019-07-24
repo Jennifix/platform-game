1e3b965433e7e376eca8d6cb90978091af28ab4c
Jenni <christoffersson.jenni@gmail.com>
2019-07-24 22:08:52 +0200

    New attempt

---
+++ static/js/platforms.js
@@ -0,0 +1,103 @@
+var platforms = [];
+
+platforms.push({
+    x: 0,
+    y: 350,
+    width: 290,
+    height: 110
+});
+
+platforms.push({
+    x: 315,
+    y: 350,
+    width: 50,
+    height: 110
+});
+
+platforms.push({
+    x: 390,
+    y: 350,
+    width: 150,
+    height: 110
+});
+
+platforms.push({
+    x: 700,
+    y: 350,
+    width: 300,
+    height: 110
+});
+
+//in the air, from left to right
+
+platforms.push({
+    x: 120,
+    y: 290,
+    width: 50,
+    height: 10
+});
+
+platforms.push({
+    x: 180,
+    y: 250,
+    width: 50,
+    height: 10
+});
+
+platforms.push({
+    x: 550,
+    y: 290,
+    width: 20,
+    height: 10
+});
+
+platforms.push({
+    x: 590,
+    y: 250,
+    width: 50,
+    height: 10
+});
+
+platforms.push({
+    x: 700,
+    y: 140,
+    width: 40,
+    height: 10
+});
+
+platforms.push({
+    x: 740,
+    y: 100,
+    width: 10,
+    height: 50
+});
+
+platforms.push({
+    x: 760,
+    y: 290,
+    width: 50,
+    height: 10
+});
+
+platforms.push({
+    x: 800,
+    y: 120,
+    width: 50,
+    height: 10
+});
+
+platforms.push({
+    x: 850,
+    y: 250,
+    width: 50,
+    height: 10
+});
+
+platforms.push({
+    x: 920,
+    y: 190,
+    width: 50,
+    height: 10
+});
+
+var platformTop = platforms[i].
--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,5 @@
+
+
 (function () {
     var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
     window.requestAnimationFrame = requestAnimationFrame;
@@ -12,79 +14,29 @@ var canvasHeight = 460;
 
 var player = {
     x: canvasWidth - 990,
-    y: canvasHeight - 30,
+    y: canvasHeight - 150,
     width: 10,
     height: 30,
     speed: 2,
     velX: 0,
     velY: 0,
-    friction: 0.8,
-    gravity: 0.3,
-    jumping: false
 };
 
-var keys = [];
+function draw(x, y, width, height, color) {
+    ctx.fillStyle = color;
+    ctx.fillRect(x, y, width, height,);
+}
 
+function update() { 
+    draw(player.x, player.y, player.width, player.height, "red");
 
-
-
-function update() {
-
-    ctx.clearRect(0, 0, canvasWidth, canvasHeight);
-    ctx.fillStyle = "red";
-    ctx.fillRect(player.x, player.y, player.width, player.height);
-
-    // creating arrow interactions
-    //up
-    if (keys[38]) {
-        if (!player.jumping) {
-            player.jumping = true;
-            player.velY = -player.gravity*30;
+    for (var i = 0; i < platforms.length; i++) {
+        draw(platforms[i].x, platforms[i].y, platforms[i].width, platforms[i].height, "white");
         }
-    }
-    //right
-    if (keys[39]) {
-        if (player.velX < player.speed) {
-            player.velX++;
-        }
-    }
-    //left
-    if (keys[37]) {
-        if (player.velX > -player.speed) {
-            player.velX--;
-        }
-    }
-
-    player.velX *= player.friction;
-
-    player.velY += player.gravity;
-    
-    player.x += player.velX;
-    player.y += player.velY;
-
-    if (player.x >= canvasWidth - player.width) {
-        player.x = canvasWidth - player.width;
-    } else if (player.x <= 0) {
-        player.x = 0;
-    }
-
-    if (player.y >= canvasHeight - player.height) {
-        player.y = canvasHeight - player.height;
-        player.jumping = false;}
-
     
 
-    window.requestAnimationFrame(update);
-};
+    requestAnimationFrame(update);
+}
 
-window.addEventListener("load",function(){
-    update();
-});
+update();
 
-document.body.addEventListener("keydown", function (e) {
-    keys[e.keyCode] = true;
-});
-
-document.body.addEventListener("keyup", function (e) {
-    keys[e.keyCode] = false;
-});
